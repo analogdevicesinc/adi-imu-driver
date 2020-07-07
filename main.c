3ced839aa12312cc70ae6cecab5e138f91f18a2a
Sundar Palani <Sundar.Palani@analog.com>
2020-07-07 01:29:07 -0400

    printing pitch/roll from accel outputs

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "adi_imu_driver.h"
 #include "spi_driver.h"
 
+#include <math.h>
 #define PI 3.141592653589793238
 
 int main()
@@ -36,14 +37,18 @@ int main()
 
     printf("\nPerforming burst read..\n");
     adi_imu_BurstOutput_t out;
-    char imu_out[200];
     for (int i=0; i<10; i++){
         if ((ret = adi_imu_ReadBurst(&imu, &out)) < 0) return -1;
-        sprintf(imu_out, "sys_status=%x, temp=%fC, accX=%f, accY=%f, accZ=%f, gyroX=%f, gyroY=%f, gyroZ=%f datacnt_Or_ts=%d crc=0x%x\n", out.sysEFlag, 25 + out.tempOut * tempLSB, out.accl.x * acclLSB, out.accl.y * acclLSB, out.accl.z * acclLSB, out.gyro.x * gyroLSB, out.gyro.y * gyroLSB, out.gyro.z * gyroLSB, out.dataCntOrTimeStamp, out.crc);
-        printf("%s\n", imu_out);
+        printf("\nsys_status=%x, temp=%f\u2103, accX=%f, accY=%f, accZ=%f, gyroX=%f, gyroY=%f, gyroZ=%f datacnt_Or_ts=%d crc=0x%x\n", out.sysEFlag, 25 + out.tempOut * tempLSB, out.accl.x * acclLSB, out.accl.y * acclLSB, out.accl.z * acclLSB, out.gyro.x * gyroLSB, out.gyro.y * gyroLSB, out.gyro.z * gyroLSB, out.dataCntOrTimeStamp, out.crc);
+        double accelX = out.accl.x * acclLSB;
+        double accelY = out.accl.y * acclLSB;
+        double accelZ = out.accl.z * acclLSB;
+        printf("Pitch = %f deg \n", 180 * atan2(accelX, sqrt(accelY*accelY + accelZ*accelZ))/PI);
+        printf("Roll = %f deg\n", 180 * atan2(accelY, sqrt(accelX*accelX + accelZ*accelZ))/PI);
         adi_imu_DelayMicroSeconds(10000);
     }
-
+    printf("\n");
+    
     /* 5. Perform self test and display results*/
     if ((ret = adi_imu_PerformSelfTest(&imu)) < 0) return -1;
 
